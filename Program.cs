47fa7d9f9e619a8de01c8cf2dd3be553aa90980b
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2023-09-26 14:13:19 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using LR1;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//INTRO
+app.MapGet("/", () => "Welcome to LR1");
+
+//TASK 1
+app.MapGet("/task1", async (context) =>
+{
+    Company company = new Company();
+    company.Name = "Google";
+    company.Workers = 187000;
+    await context.Response.WriteAsync($"{company.GetInfo()}");
+});
+
+//TASK 2
+app.MapGet("/task2", async (context) =>
+{
+    Random rnd = new Random();
+    int result = rnd.Next(1, 101);
+    await context.Response.WriteAsync($"Random number: {result}");
+});
+
+app.Run(); 
