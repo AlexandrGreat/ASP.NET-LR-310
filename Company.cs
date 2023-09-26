b00ca188c22d2beea09d0061df858e356455d5ce
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2023-09-26 14:13:51 +0300

    Create Company.cs

---
+++ Company.cs
@@ -0,0 +1,12 @@
+﻿namespace LR1
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public int Workers { get; set; }
+
+        public string GetInfo() {
+            return $"{Name}: {Workers} workers";
+        }
+    }
+}
