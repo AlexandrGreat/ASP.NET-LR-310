e28dadd2cb83deac1b0f4d28973a70e288cb5d71
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2023-11-17 10:42:08 +0200

    Add files via upload

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LR12.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<UserModel> Users { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
